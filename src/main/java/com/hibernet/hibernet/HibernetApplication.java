8fbdd689a2e1ff1bc87ae2d0684e495c5deffee0
Deepak1jha <ddjha171@gmail.com>
2021-09-12 14:34:36 +0530

    session created

--- src/main/java/com/hibernet/hibernet/HibernetApplication.java
+++ src/main/java/com/hibernet/hibernet/HibernetApplication.java
@@ -1,6 +1,9 @@
 package com.hibernet.hibernet;
 
+import com.hibernet.hibernet.model.student.Student;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +14,13 @@ public class HibernetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HibernetApplication.class, args);
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		System.out.println("******************************************************");
-		System.out.println("******************************************************");
-		System.out.println("******************************************************");
-		System.out.println("******************************************************");
-		System.out.println(sessionFactory);
-
-	}
+		Student student = new Student("name","Delhi",22);
+		System.out.println(student);
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(student);
+		transaction.commit();
+		session.close();
+ 	}
 
 }
---
+++ src/main/java/com/hibernet/hibernet/model/student/Student.java
@@ -0,0 +1,26 @@
+package com.hibernet.hibernet.model.student;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity(name = "student_details")
+@Data
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String city;
+
+    public Student( String name, String city,int id) {
+        this.name = name;
+        this.city = city;
+        this.id=id;
+    }
+}
